eab72a238d75f3fe8aadd108be948152064c2f05
Victor Bollentini <victorboll@yahoo.com.br>
2024-06-25 11:05:51 -0300

    Atualizacao do menu principal

--- src/view/contato/MainView.java
+++ src/view/contato/MainView.java
@@ -1,60 +1,46 @@
-package view.contato;
-
 import java.util.Scanner;
 
 public class MainView {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean menuAtivo = true;
-        
-        //utilizar um do while. Ter 4 opções, insere, lista, altera, remove e sair. Ter uma view para cada opção
-        while (menuAtivo) {
-            System.out.println("Escolha uma opcao:");
-            System.out.println("1. Inserir contato");
-            System.out.println("2. Listar toda tabela");
-            System.out.println("3. Listar nomes comecam com letra");
-            System.out.println("4. Lista contato por id");
-            System.out.println("5. Alterar os dados nome e/ou email e/ou endereco por id");
-            System.out.println("6. Remover contato por id");
-            System.out.println("Digite 'x' para terminar o programa.");
+        String opcao;
+         
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Adicionar contato");
+            System.out.println("2. Visualizar contato");
+            System.out.println("3. Alterar contato");
+            System.out.println("4. Deletar contato");
+            System.out.println("5. Sair do programa");
             System.out.print("Digite sua escolha: ");
-            String opcao = scanner.nextLine();
+            opcao = scanner.nextLine();
 
             switch (opcao) {
                 case "1":
-                    System.out.println("Executando Opcao 1...");
-                    // Coloque o código da Ação 1 aqui
+                    System.out.println("Adicionando contato...");
+                    // Chamar a classe de adição de contato
                     break;
                 case "2":
-                    System.out.println("Executando Opcao 2...");
-                    // Coloque o código da Ação 2 aqui
+                    System.out.println("Visualizando contatos...");
+                    // Chamar a classe de visualização de contatos
                     break;
                 case "3":
-                    System.out.println("Executando Opcao 3...");
-                    // Coloque o código da Ação 3 aqui
+                    System.out.println("Alterando contato...");
+                    // Chamar a classe de alteração de contato
                     break;
                 case "4":
-                    System.out.println("Executando Opcao 4...");
-                    // Coloque o código da Ação 4 aqui
+                    System.out.println("Deletando contato...");
+                    // Chamar a classe de remoção de contato
                     break;
                 case "5":
-                    System.out.println("Executando Opcao 5...");
-                    // Coloque o código da Ação 5 aqui
-                    break;
-                case "6":
-                    System.out.println("Executando Opcao 6...");
-                    // Coloque o código da Ação 5 aqui
-                    break;
-                case "x":
-                	menuAtivo = false;
                     System.out.println("Encerrando o programa...");
                     break;
                 default:
                     System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
                     break;
             }
-        }
+        } while (!opcao.equals("5"));
 
         scanner.close();
     }
