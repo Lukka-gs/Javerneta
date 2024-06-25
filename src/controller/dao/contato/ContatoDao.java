cfab2e8bb4c0696de5ed6ab9183a389ff630b010
Daniela <danisegd@hotmail.com>
2024-06-25 12:09:01 -0300

    Cria métodos de busca de contato por ID e lista por letra

--- src/controller/dao/contato/ContatoDao.java
+++ src/controller/dao/contato/ContatoDao.java
@@ -51,4 +51,49 @@ public class ContatoDao {
 		return contatos;
 	}
 
+	public List<Contato> getListaPorLetra(String letra) throws SQLException{
+		String query = "select * from contatos where nome like ?";
+		PreparedStatement stmt = con.prepareStatement(query);
+		stmt.setString(1, letra + "%");
+
+		ResultSet rset = stmt.executeQuery();
+		List<Contato> contatos = new ArrayList<Contato>();
+
+		while (rset.next()) {
+			Contato contato = new Contato();
+			contato.setNome(rset.getString("nome"));
+			contato.setEmail(rset.getString("email"));
+			contato.setEndereco(rset.getString("endereco"));
+			contatos.add(contato);
+		}
+
+		rset.close();
+		stmt.close();
+		con.close();
+
+		return contatos;
+	}
+
+	public Contato getContatoPorId(String Id) throws SQLException{
+		String query = "select * from contatos where id = ?";
+		PreparedStatement stmt = con.prepareStatement(query);
+		stmt.setString(1, Id);
+
+		ResultSet rset = stmt.executeQuery();
+		Contato contato = null;
+
+		if (rset.next()) {
+			contato = new Contato();
+			contato.setNome(rset.getString("nome"));
+			contato.setEmail(rset.getString("email"));
+			contato.setEndereco(rset.getString("endereco"));
+		}
+
+		rset.close();
+		stmt.close();
+		con.close();
+
+		return contato;
+	}
+
 }
